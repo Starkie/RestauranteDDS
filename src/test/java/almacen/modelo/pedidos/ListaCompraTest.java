961a65076b3bc4ba070b8f6b60d8fecb588b5802
Adriano Vega <adrianovega@hotmail.com>
2017-05-13 19:57:21 +0200

    Updated tests for ListaCompra

--- src/test/java/almacen/modelo/pedidos/ListaCompraTest.java
+++ src/test/java/almacen/modelo/pedidos/ListaCompraTest.java
@@ -7,43 +7,41 @@ public class ListaCompraTest {
     @Test
     public void ShouldReturnSumOfAllElementsPrice() throws Exception {
 
-        ListaCompra base = new ListaCompuesto(1, "Lista 1", "Ejemplo");
+        ListaCompra base = new ListaCompuesto("Lista 1", "Ejemplo");
         Assert.assertEquals("El precio de la lista vacia es 0", 0.0, base.getPrecio(), 0.1);
 
-        ListaCompra elemento = new ListaElemento(1, "Elemento 1", "Elemento 1", 300);
+        ListaCompra elemento = new ListaElemento("Elemento 1", "Elemento 1", 1, 300);
         Assert.assertEquals("El precio de elemento1 es 3 €", 3.0, elemento.getPrecio(), 0.1);
 
-        ListaCompra elemento2 = new ListaElemento(2, "Elemento 2", "Elemento 2", 800);
+        ListaCompra elemento2 = new ListaElemento("Elemento 2", "Elemento 2", 2, 800);
 
         base.add(elemento);
         base.add(elemento2);
 
-        Assert.assertEquals("El precio de Lista 1 con 2 elementos es 11 €", 11.0, base.getPrecio(), 0.1);
+        Assert.assertEquals("El precio de Lista 1 con 2 elementos es 19 €", 19.0, base.getPrecio(), 0.1);
 
-        ListaCompra lista2 = new ListaCompuesto(4, "Lista 2", "Ejemplo");
-        ListaCompra elemento3 = new ListaElemento(5, "Elemento 3", "Elemento 3", 600);
+        ListaCompra lista2 = new ListaCompuesto("Lista 2", "Ejemplo");
+        ListaCompra elemento3 = new ListaElemento("Elemento 3", "Elemento 3", 2, 600);
         lista2.add(elemento3);
 
         base.add(lista2);
-        Assert.assertEquals("El precio de Lista 1 con 2 elementos y una lista es 17 €", 17.0, base.getPrecio(), 0.1);
+        Assert.assertEquals("El precio de Lista 1 con 2 elementos y una lista es 31 €", 31.0, base.getPrecio(), 0.1);
 
         base.remove(elemento);
-        Assert.assertEquals("Al eliminar elemento, el precio es de 14€", 14.0, base.getPrecio(), 0.1);
-
-
+        Assert.assertEquals("Al eliminar elemento, el precio es de 28€", 28.0, base.getPrecio(), 0.1);
     }
 
     @Test(expected=UnsupportedOperationException.class)
     public void ShouldThrowUnsupportedOperationOnAddInElement() throws Exception {
-        ListaCompra elemento = new ListaElemento(1, "elemento", "elemento", 200);
-        ListaCompra elemento2 = new ListaElemento(1, "elemento", "elemento", 200);
+        ListaCompra elemento = new ListaElemento("elemento", "elemento", 2, 200);
+        ListaCompra elemento2 = new ListaElemento("elemento", "elemento", 2, 200);
         elemento.add(elemento2);
     }
 
     @Test(expected=UnsupportedOperationException.class)
     public void ShouldThrowUnsupportedOperationOnRemoveInElement() throws Exception {
-        ListaCompra elemento = new ListaElemento(1, "elemento", "elemento", 200);
-        ListaCompra elemento2 = new ListaElemento(1, "elemento", "elemento", 200);
+        ListaCompra elemento = new ListaElemento("elemento", "elemento", 2, 200);
+        ListaCompra elemento2 = new ListaElemento("elemento", "elemento", 2, 200);
         elemento.remove(elemento2);
     }
 
