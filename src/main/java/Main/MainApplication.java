06eb67151c9504cb3581fc15914740f9f2e7baac
Adriano Vega <adrianovega@hotmail.com>
2017-05-13 15:09:11 +0200

    Added some more tests in main meanwhile

--- src/main/java/Main/MainApplication.java
+++ src/main/java/Main/MainApplication.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import model.Alimento;
+import model.Categoria;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import persistance.AlimentoService;
 import persistance.AppContext;
+import persistance.CategoriaService;
 
 @SpringBootApplication
 @ComponentScan({"persistance"})
@@ -22,9 +24,14 @@ public class MainApplication extends Application{
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
-        AlimentoService alimentoService = (AlimentoService) AppContext.getBean("alimento");
+        AlimentoService alimentoService = (AlimentoService) AppContext.getBean("alimentoService");
         alimentoService.add(new Alimento("manzana", "fruta"));
         Alimento a2 = alimentoService.findByName("manzana");
+        a2.addCategoria(new Categoria("categoria", "cat"));
+        CategoriaService categoriaService = (CategoriaService) AppContext.getBean("categoriaService");
+        alimentoService.update(a2);
+        Categoria cat = categoriaService.findByName("categoria");
+
         launch(args);
     }
 
