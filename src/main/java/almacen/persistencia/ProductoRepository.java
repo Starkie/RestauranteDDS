3fdcca861e778553636d2e20ed0e5d0a6dddee20
Adriano Vega <adrianovega@hotmail.com>
2017-05-23 19:53:47 +0200

    Created the DAO for Producto

---
+++ src/main/java/almacen/persistencia/ProductoRepository.java
@@ -0,0 +1,9 @@
+package almacen.persistencia;
+
+import almacen.modelo.Producto;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends CrudRepository<Producto, Long> {
+}
