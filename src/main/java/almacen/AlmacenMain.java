c3e638f5364f64643a6c5fb43c8269e834f71464
Adriano Vega <adrianovega@hotmail.com>
2017-06-10 22:18:14 +0200

    Updated AlmacenMain to use ProductoAlmacen

--- src/main/java/almacen/AlmacenMain.java
+++ src/main/java/almacen/AlmacenMain.java
@@ -1,8 +1,14 @@
 package almacen;
 
 import almacen.model.Producto;
+import almacen.model.ProductoAlmacen;
+import almacen.persistance.ProductoAlmacenService;
 import almacen.persistance.ProductoService;
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -11,9 +17,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import persistance.AppContext;
@@ -21,6 +25,7 @@ import persistance.AppContext;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class AlmacenMain implements Initializable {
@@ -29,11 +34,17 @@ public class AlmacenMain implements Initializable {
     @FXML
     private TableView tablaProductos;
     @FXML
-    private TableColumn<Producto, String> nombreColumn;
+    private TableColumn<ProductoAlmacen, String> nombreColumn;
     @FXML
-    private TableColumn<Producto, String> alimentoColumn;
+    private TableColumn<ProductoAlmacen, String> alimentoColumn;
     @FXML
-    private TableColumn<Producto, Integer> stockColumn;
+    private TableColumn<ProductoAlmacen, Number> cantidadColumn;
+    @FXML
+    private TableColumn<ProductoAlmacen, Number> stockColumn;
+    @FXML
+    private TableColumn<ProductoAlmacen, String> unidadColumn;
+    @FXML
+    private TableColumn<ProductoAlmacen, Number> precioColumn;
 
     @FXML
     private Button nuevoProductoButton;
@@ -41,23 +52,70 @@ public class AlmacenMain implements Initializable {
     private Button editarProductoButton;
     @FXML
     private Button borrarProductoButton;
+    @FXML
+    private Button pedidosButton;
 
 
-    private ObservableList<Producto> productos;
+    private ObservableList<ProductoAlmacen> productos;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        ProductoService productoService = (ProductoService) AppContext.getBean("productoService");
-        ArrayList<Producto> listaProductos = new ArrayList<>();
+        ProductoAlmacenService productoService = (ProductoAlmacenService) AppContext.getBean("productoAlmacenService");
+        ArrayList<ProductoAlmacen> listaProductos = new ArrayList<>();
         productoService.findAll().forEach(p -> listaProductos.add(p));
 
         productos = FXCollections.observableArrayList(listaProductos);
 
-        nombreColumn.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().getNombre()));
+        nombreColumn.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().getProducto().getNombre()));
 
-        alimentoColumn.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().getAlimento().getNombre()));
+        alimentoColumn.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().getProducto().getAlimento().getNombre()));
+
+        cantidadColumn.setCellValueFactory(p -> new SimpleDoubleProperty(p.getValue().getProducto().getCantidad()));
+
+        unidadColumn.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().getProducto().getUnidades().toString()));
+
+        stockColumn.setCellValueFactory(p -> new SimpleIntegerProperty(p.getValue().getStock()));
+
+        precioColumn.setCellValueFactory(p -> new SimpleDoubleProperty(p.getValue().getProducto().getPrecio()));
 
         tablaProductos.setItems(productos);
 
+        editarProductoButton.disableProperty().bind(Bindings.isEmpty(tablaProductos.getSelectionModel().getSelectedItems()));
+
+        borrarProductoButton.disableProperty().bind(Bindings.isEmpty(tablaProductos.getSelectionModel().getSelectedItems()));
     }
+
+    @FXML
+    private void OnBorrarProductoClick() {
+        ProductoAlmacen producto = (ProductoAlmacen) tablaProductos.getSelectionModel().getSelectedItem();
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Eliminar Producto");
+        alert.setHeaderText("Eliminar Producto: " + producto.getProducto().getNombre());
+        alert.setContentText("¿Desea eliminar el producto?");
+        Optional<ButtonType> resultado = alert.showAndWait();
+        if(resultado.get() == ButtonType.OK) {
+            ProductoAlmacenService productoAlmacenService = (ProductoAlmacenService) AppContext.getBean("productoAlmacenService");
+            productoAlmacenService.remove(producto);
+            Alert alert2 = new Alert(Alert.AlertType.INFORMATION);
+            alert2.setTitle("Producto Eliminado");
+            alert2.setHeaderText("Producto Elminado Correctamente");
+            alert2.showAndWait();
+
+            ArrayList<ProductoAlmacen> listaProductos = new ArrayList<>();
+            productoAlmacenService.findAll().forEach(p -> listaProductos.add(p));
+            productos = FXCollections.observableArrayList(listaProductos);
+            tablaProductos.setItems(productos);
+        }
+    }
+    @FXML
+    private void OnNuevoProductoClick() {
+
+    }
+
+    @FXML
+    private void OnEditarProductoClick() {
+
+    }
+
 }
