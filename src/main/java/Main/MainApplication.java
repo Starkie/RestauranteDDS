058835a3dc4b2e499bfe765bbffb2f1f37f6142f
Adriano Vega <adrianovega@hotmail.com>
2017-06-10 09:27:18 +0200

    Added Almacen to the entity and component scans

--- src/main/java/Main/MainApplication.java
+++ src/main/java/Main/MainApplication.java
@@ -5,33 +5,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import model.Alimento;
-import model.Categoria;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import persistance.AlimentoService;
-import persistance.AppContext;
-import persistance.CategoriaService;
 
 @SpringBootApplication
-@ComponentScan({"persistance"})
-@EntityScan({"model"})
-@EnableJpaRepositories({"persistance"})
+@ComponentScan({"persistance", "almacen.persistance"})
+@EntityScan({"model", "almacen.model"})
+@EnableJpaRepositories({"persistance", "almacen.persistance"})
 public class MainApplication extends Application{
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
-        AlimentoService alimentoService = (AlimentoService) AppContext.getBean("alimentoService");
-        alimentoService.add(new Alimento("manzana", "fruta"));
-        Alimento a2 = alimentoService.findByName("manzana");
-        a2.addCategoria(new Categoria("categoria", "cat"));
-        CategoriaService categoriaService = (CategoriaService) AppContext.getBean("categoriaService");
-        alimentoService.update(a2);
-        Categoria cat = categoriaService.findByName("categoria");
-
         launch(args);
     }
 
