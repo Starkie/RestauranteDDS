73a5ba7c5e3eb8a1971371b46a66081e3156ad86
Adriano Vega <adrianovega@hotmail.com>
2017-06-10 16:52:08 +0200

    Removed Proveedor aiming to reduce the scope

--- src/main/java/almacen/model/Producto.java
+++ src/main/java/almacen/model/Producto.java
@@ -13,25 +13,22 @@ public class Producto {
     private String descripcion;
     @ManyToOne
     private Alimento alimento;
-    @ManyToOne(cascade = CascadeType.MERGE, optional = false)
-    private Proveedor proveedor;
     private int precio;
     private double cantidad;
     private UnidadesCantidad unidades;
 
     public Producto() {}
 
-    public Producto(String nombre, Alimento alimento, Proveedor proveedor, int precio, double cantidad, UnidadesCantidad unidades) {
+    public Producto(String nombre, Alimento alimento, int precio, double cantidad, UnidadesCantidad unidades) {
         this.nombre = nombre;
         this.alimento = alimento;
-        this.proveedor = proveedor;
         this.precio = precio;
         this.cantidad = cantidad;
         this.unidades = unidades;
     }
 
-    public Producto(String nombre, String descripcion, Alimento alimento, Proveedor proveedor, int precio, double cantidad, UnidadesCantidad unidades) {
-        this(nombre, alimento, proveedor, precio, cantidad, unidades);
+    public Producto(String nombre, String descripcion, Alimento alimento, int precio, double cantidad, UnidadesCantidad unidades) {
+        this(nombre, alimento, precio, cantidad, unidades);
         this.descripcion = descripcion;
     }
 
@@ -58,14 +55,6 @@ public class Producto {
 
     public void setAlimento(Alimento alimento) {
         this.alimento = alimento;
-    }
-
-    public Proveedor getProveedor() {
-        return proveedor;
-    }
-
-    public void setProveedor(Proveedor proveedor) {
-        this.proveedor = proveedor;
     }
 
     public int getPrecio() {
