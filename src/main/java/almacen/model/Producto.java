9a7a954145a349fee91b2e4f1c5c64035aae7421
Adriano Vega <adrianovega@hotmail.com>
2017-06-11 11:19:29 +0200

    Changed setPrecio to double

--- src/main/java/almacen/model/Producto.java
+++ src/main/java/almacen/model/Producto.java
@@ -46,7 +46,7 @@ public class Producto {
         return precio;
     }
 
-    public void setPrecio(int precio) {
+    public void setPrecio(double precio) {
         this.precio = precio;
     }
 
