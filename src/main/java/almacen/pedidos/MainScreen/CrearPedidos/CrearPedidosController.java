469ed81b1a6aef46e26bd9c6ba760e6bd97c0f24
Adriano Vega <adrianovega@hotmail.com>
2017-06-11 19:31:32 +0200

    Add Product to Pedido and Confirm Pedido functionalities added

--- src/main/java/almacen/pedidos/MainScreen/CrearPedidos/CrearPedidosController.java
+++ src/main/java/almacen/pedidos/MainScreen/CrearPedidos/CrearPedidosController.java
@@ -5,17 +5,20 @@ import almacen.controllers.ProductoController;
 import almacen.model.Producto;
 import almacen.pedidos.controllers.GestorPedidos;
 import almacen.pedidos.model.Pedido;
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
 
 import java.net.URL;
+import java.text.DecimalFormat;
+import java.text.ParsePosition;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class CrearPedidosController implements Initializable{
@@ -54,6 +57,8 @@ public class CrearPedidosController implements Initializable{
 
         gestorPedidos = GestorPedidos.getInstance();
 
+        addProductoButton.disableProperty().bind(Bindings.isEmpty(tablaProductos.getSelectionModel().getSelectedItems()));
+
         productos = FXCollections.observableArrayList(productoController.getAllProductos());
 
         nombreColumn.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().getNombre()));
@@ -76,7 +81,40 @@ public class CrearPedidosController implements Initializable{
 
     @FXML
     public void OnAddProductoClick() {
+        Optional<String> result = promptUnidadesProducto();
+        if (result.isPresent()){
+            Producto producto = (Producto) tablaProductos.getSelectionModel().getSelectedItem();
+            gestorPedidos.addProductoToPedido(pedido, producto, (int) Double.parseDouble(result.get()));
+        }
+    }
 
+    public Optional<String> promptUnidadesProducto() {
+        TextInputDialog dialog = new TextInputDialog("1");
+        dialog.setTitle("Añadir Producto al Pedido");
+        dialog.setHeaderText("Añadir Producto al Pedido");
+        dialog.setContentText("Unidades:");
+        DecimalFormat format = new DecimalFormat( "#.0" );
+        dialog.getEditor().setTextFormatter( new TextFormatter<>(c ->
+        {
+            if ( c.getControlNewText().isEmpty() )
+            {
+                return c;
+            }
+
+            ParsePosition parsePosition = new ParsePosition( 0 );
+            Object object = format.parse( c.getControlNewText(), parsePosition );
+
+            if ( object == null || parsePosition.getIndex() < c.getControlNewText().length() )
+            {
+                return null;
+            }
+            else
+            {
+                return c;
+            }
+        }));
+
+        return dialog.showAndWait();
     }
 
     @FXML
@@ -86,6 +124,18 @@ public class CrearPedidosController implements Initializable{
 
     @FXML
     public void OnConfirmarPedidoClick() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmar Pedido");
+        alert.setHeaderText("Confirmar Pedido");
+        alert.setContentText("¿Desea confirmar el pedido y salir?");
+        Optional<ButtonType> resultado = alert.showAndWait();
 
+        if(resultado.get() == ButtonType.OK) {
+            gestorPedidos.guardarPedido(pedido);
+
+            Stage stage = (Stage) confirmarPedidoButton.getScene().getWindow();
+
+            stage.close();
+        }
     }
 }
