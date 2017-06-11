b1d5322e00c78ee91c24cd1fe94e355dc1a188e0
Adriano Vega <adrianovega@hotmail.com>
2017-06-11 23:32:48 +0200

    Updated Tests to reflect changes on ListaCompra

--- src/test/java/almacen/pedidos/model/PedidoTest.java
+++ src/test/java/almacen/pedidos/model/PedidoTest.java
@@ -1,16 +1,34 @@
 package almacen.pedidos.model;
 
+import almacen.model.Producto;
+import almacen.model.UnidadesCantidad;
+import model.Alimento;
+import org.junit.Before;
 import org.junit.Test;
 
 public class PedidoTest {
+
+    Producto p1;
+    Producto p2;
+    ListaCompra listaCompra;
+    Pedido pedido;
+
+    @Before
+    public void setUp() {
+        Alimento alimento = new Alimento();
+        p1 = new Producto("Prod1", alimento, 3.00, 2, UnidadesCantidad.KG);
+        p2 = new Producto("Prod2", alimento, 8.00, 2, UnidadesCantidad.KG);
+        listaCompra = new ListaElemento(p1, 1 );
+        pedido = new Pedido(listaCompra);
+    }
 
 
     @Test
     public void shouldReturn() throws Exception {
         ListaCompra base = new ListaCompuesto("Lista 1", "Ejemplo");
-        ListaCompra elemento = new ListaElemento("Elemento 1", "Elemento 1", 1, 300);
+        ListaCompra elemento = new ListaElemento(p1, 1);
 
-        ListaCompra elemento2 = new ListaElemento("Elemento 2", "Elemento 2", 2, 800);
+        ListaCompra elemento2 = new ListaElemento(p2, 2);
         base.add(elemento);
         base.add(elemento2);
 
@@ -19,44 +37,32 @@ public class PedidoTest {
 
     @Test
     public void Should_Not_Throw_EstadoPedidoInvalido_When_Confirming_Pendiente_Order() throws Exception {
-        ListaCompra listaCompra = new ListaElemento("Elemento 1", "Elemento 1", 1, 300);
-        Pedido pedido = new Pedido(listaCompra);
         pedido.confirmarPedido();
     }
 
     @Test
     public void Should_Not_Throw_EstadoPedidoInvalido_When_Receiving_Order() throws Exception {
-        ListaCompra listaCompra = new ListaElemento("Elemento 1", "Elemento 1", 1, 300);
-        Pedido pedido = new Pedido(listaCompra);
         pedido.confirmarPedido();
         pedido.recibirPedido();
     }
 
     @Test
     public void Should_Not_Throw_EstadoPedidoInvalido_When_Canceling_Not_Completed_Order() throws Exception {
-        ListaCompra listaCompra = new ListaElemento("Elemento 1", "Elemento 1", 1, 300);
-        Pedido pedido = new Pedido(listaCompra);
         pedido.cancelarPedido();
     }
 
     @Test(expected = EstadoPedidoInvalidoException.class)
     public void Should_Throw_EstadoPedidoInvalidoException_When_Confirming_Canceled_Order() throws Exception {
-        ListaCompra listaCompra = new ListaElemento("Elemento 1", "Elemento 1", 1, 300);
-        Pedido pedido = new Pedido(listaCompra);
         pedido.cancelarPedido();
         pedido.confirmarPedido();
     }
 
     @Test(expected = EstadoPedidoInvalidoException.class)
     public void Should_Throw_EstadoPedidoInvalidoException_When_Canceling_Completed_Order() throws Exception {
-        ListaCompra listaCompra = new ListaElemento("Elemento 1", "Elemento 1", 1, 300);
-        Pedido pedido = new Pedido(listaCompra);
         pedido.confirmarPedido();
         pedido.recibirPedido();
         pedido.cancelarPedido();
     }
-
-
 
 
 }
