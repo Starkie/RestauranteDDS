c361c65568c7b5e99011a1adbb3c92f39effd3c8
Adriano Vega <adrianovega@hotmail.com>
2017-06-11 11:18:58 +0200

    Added Entity Controllers to ProductoAlmacen and Alimento

---
+++ src/main/java/almacen/ProductoAlmacenController.java
@@ -0,0 +1,33 @@
+package almacen;
+
+import almacen.model.ProductoAlmacen;
+import almacen.persistance.ProductoAlmacenService;
+import persistance.AppContext;
+
+import java.util.ArrayList;
+
+public class ProductoAlmacenController {
+    private static ProductoAlmacenController productosController;
+    private static ProductoAlmacenService productoAlmacenService;
+
+    private ProductoAlmacenController() {
+        this.productosController = this;
+        this.productoAlmacenService = (ProductoAlmacenService) AppContext.getBean("productoAlmacenService");
+    }
+
+    public static ProductoAlmacenController getInstance() {
+        if(productosController == null)
+            productosController = new ProductoAlmacenController();
+        return productosController;
+    }
+
+    public void guardarProducto(ProductoAlmacen productoAlmacen) {
+        productoAlmacenService.update(productoAlmacen);
+    }
+
+    public ArrayList<ProductoAlmacen> getAllProductos() {
+        ArrayList<ProductoAlmacen> lista = new ArrayList<>();
+        productoAlmacenService.findAll().forEach(p -> lista.add(p));
+        return lista;
+    }
+}
