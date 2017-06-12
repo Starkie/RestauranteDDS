337e4846ab4bf4c5cbf76164e4c08994faff73a0
Adriano Vega <Starkie@users.noreply.github.com>
2017-06-12 12:04:48 +0200

    Revert "Restaurante pedidos"

--- src/main/java/persistance/PedidoRestauranteRepository.java
+++
@@ -1,9 +0,0 @@
-package persistance;
-
-import model.PedidoRestaurante;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PedidoRestauranteRepository extends CrudRepository<PedidoRestaurante,Long> {
-}
